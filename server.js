a559623e254a7c698ef8d492d95a3518279aa7d9
Mirac Ozturk <ozturkmirac04@gmail.com>
2026-02-21 00:01:10 +0300

    port fix for render

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ app.post("/gonder", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Sunucu çalışıyor: http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Sunucu çalışıyor...");
 });
